84c258782560ead7033971f8f3a63b47acc61e00
Taichi Kosugi <no3b1219@gmail.com>
2018-09-01 16:06:09 +0900

    mp3 trial

--- learn_code.js
+++ learn_code.js
@@ -44,6 +44,7 @@ const clovaSkillHandler = clova.Client
         // 「中吉」だと「なかよし」発生されてしまう
         const how_to = ["0、3、2、0、1、0", "ミュート、0、0、2、3、2", "ミュート、0、2、0、1、0"]
         const codes = ["C", "D", "Am"]       // 日と星座を元に運勢を決定。日が変わると違う運勢に。
+        if (codes.indexOf(slots.code_names) != -1){
         const my_output = how_to[codes.indexOf(slots.code_names)]
 
         speech = {
@@ -53,6 +54,13 @@ const clovaSkillHandler = clova.Client
         }
         responseHelper.setSimpleSpeech(speech)
         responseHelper.setSimpleSpeech(speech, true)
+        
+        }
+
+        //音声の場所
+        // responseHelper.setSimpleSpeech(
+        //   clova.SpeechBuilder.createSpeechUrl('https://exsample.com/myaudio.mp3')
+        // );
 
         break;
       // ビルトインインテント。ユーザーによるインプットが使い方のリクエストと判別された場合
